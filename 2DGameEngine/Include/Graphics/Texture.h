6f0f524752b94a5f1051126c84d0ad0d35123c2a
Cybot <stancilaionutmarian@yahoo.com>
2024-11-07 02:10:39 +0200

    Created Texture class

---
+++ 2DGameEngine/Include/Graphics/Texture.h
@@ -0,0 +1,52 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+
+
+struct TextureParamaters {
+	int wrapX = GL_CLAMP_TO_BORDER; // GL_TEXTURE_WRAP_S
+	int wrapY = GL_CLAMP_TO_BORDER; // GL_TEXTURE_WRAP_T
+
+	int minFilter = GL_NEAREST; // GL_TEXTURE_MIN_FILTER
+	int magFilter = GL_NEAREST; // GL_TEXTURE_MAG_FILTER
+
+	glm::vec4 borderColor = { 0.f, 0.f, 0.f, 1.f }; // GL_TEXTURE_BORDER_COLOR
+};
+
+class Texture {
+public:
+	Texture();
+	Texture(int width, int height, GLint format = GL_RGBA8, GLenum pixelFormat=GL_UNSIGNED_INT);
+	virtual ~Texture();
+
+	void Use(int slot = 0) const;
+	static void UseDefault(int slot = 0);
+
+	unsigned int GetTextureID() const;
+
+	inline int GetHeight() const { return m_width; }
+	inline int GetWidth() const { return m_height; }
+	inline glm::ivec2 GetResolution() const;
+
+	float GetAspectRatio();
+protected:
+
+	void GenerateOGLTexture();
+
+	TextureParamaters m_texParams;
+
+	bool m_initialized = false;
+
+	unsigned int m_texture;
+	unsigned char*  m_data = nullptr;
+
+	int m_width = 32;
+	int m_height = 32;
+	int m_channels = GL_RGBA;
+
+	GLenum m_textureMode = GL_TEXTURE_2D;
+};
+
+#endif // TEXTURE_H
---
+++ 2DGameEngine/Source/Graphics/Texture.cpp
@@ -0,0 +1,74 @@
+#include "Graphics/Texture.h"
+
+Texture::Texture()
+{
+}
+
+Texture::Texture(int width, int height, GLint format, GLenum pixelFormat)
+{
+	m_width = width;
+	m_height = height;
+
+	GenerateOGLTexture();
+
+	glTexImage2D(GL_TEXTURE_2D, 0 , format, m_width, m_height, 0, m_channels, pixelFormat, m_data);
+}
+
+Texture::~Texture()
+{
+	if (m_data != nullptr)
+	{
+		delete[] m_data;
+	}
+
+	glDeleteTextures(1, &m_texture);
+}
+
+void Texture::Use(int slot) const
+{
+	assert(m_initialized);
+
+	glActiveTexture(GL_TEXTURE0 + slot);
+	glBindTexture(m_textureMode, m_texture);
+}
+
+void Texture::UseDefault(int slot)
+{
+	glActiveTexture(GL_TEXTURE0 + slot);
+	glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+unsigned int Texture::GetTextureID() const
+{
+	return m_texture;
+}
+
+glm::ivec2 Texture::GetResolution() const
+{
+	return glm::ivec2(m_width, m_height);
+}
+
+float Texture::GetAspectRatio()
+{
+	return float(m_width)/float(m_height);
+}
+
+void Texture::GenerateOGLTexture()
+{
+	m_initialized = true;
+	glGenTextures(1, &m_texture);
+	glBindTexture(GL_TEXTURE_2D, m_texture);
+
+	// Updating the Texture Parrameters:
+	{
+		// Texdture Wrap (out of bounds)
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, m_texParams.wrapX);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, m_texParams.wrapY);
+
+		glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, &m_texParams.borderColor[0]);
+
+		//Texels
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, m_texParams.minFilter);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, m_texParams.magFilter);
+	}
+}
